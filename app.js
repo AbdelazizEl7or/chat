2aabc58b9f4f64253a3b722b9b016f1b306dc6bf
new-zizo <126802265+new-zizo@users.noreply.github.com>
2023-04-01 16:40:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ router.post("/add", (req, res, next) => {
         console.log("Erorr:" + err)
     })
 })
+
 router.post("/add/:id", (req, res, next) => {
     let id = req.params.id
     console.log(req.body)
@@ -73,6 +74,42 @@ router.post("/add/:id", (req, res, next) => {
             })
     }).catch(err => {
         console.log("Erorr:" + err)
+    })
+})
+
+
+router.post("/update/:chatId/:id", (req, res, next) => {
+    let id = req.params.id
+    let chatId = req.params.chatId
+    const all = req.body
+    client.connect().then((client) => {
+        let db = client.db("chat")
+        db.collection(`chat-${chatId}`).updateOne({ id: id }, {$set:{
+            ...all
+        }})
+
+            .catch(err => {
+                console.log(err)
+            })
+    }).catch(err => {
+        console.log("Erorr:" + err)
+    }).finally(() => {
+        res.json(all)
+    })
+})
+
+router.use("/delete/:chatId/:id", (req, res, next) => {
+    let id = req.params.id
+    let chatId = req.params.chatId
+    client.connect().then((client) => {
+        let db = client.db("chat")
+        db.collection(`chat-${chatId}`).deleteOne({ id: id })
+            .catch(err => {
+                console.log(err)
+            })
+    }).catch(err => {
+        console.log("Erorr:" + err)
+    }).finally(() => {
     })
 })
 
